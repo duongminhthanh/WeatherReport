5a3d6f826d9c7047a094902764c054b3250a29de
duongminhthanh <62210826+duongminhthanh@users.noreply.github.com>
2022-05-04 20:36:07 +0700

    khong do du lieu dc

--- app/src/main/java/com/example/weatherreport/MainActivity.java
+++ app/src/main/java/com/example/weatherreport/MainActivity.java
@@ -16,54 +16,95 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     EditText edSearch;
-    Button btnSearch,btnChangeActivity;
-    TextView txtName,txtCountry,txtTemp,txtStatus,txtHumidity,txtCloud,txtWind,txtDay;
+    Button btnSearch, btnChangeActivity;
+    TextView txtName, txtCountry, txtTemp, txtStatus, txtHumidity, txtCloud, txtWind, txtDay;
     ImageView imgIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        edSearch=(EditText) findViewById(R.id.edSearch);
-        btnSearch=(Button) findViewById(R.id.btnSearch);
-        btnChangeActivity=(Button) findViewById(R.id.btnChangActivity);
-        txtName=(TextView) findViewById(R.id.txtName);
-        txtCountry=(TextView) findViewById(R.id.txtCountry);
-        txtTemp=(TextView) findViewById(R.id.txtTemp);
-        txtStatus=(TextView) findViewById(R.id.txtStatus);
-        txtHumidity=(TextView) findViewById(R.id.txtHumidity);
-        txtCloud=(TextView) findViewById(R.id.txtCloud);
-        txtWind=(TextView) findViewById(R.id.txtWind);
-        txtDay=(TextView) findViewById(R.id.txtDay);
-        imgIcon=(ImageView) findViewById(R.id.imgIcon);
+        edSearch = (EditText) findViewById( R.id.edSearch );
+        btnSearch = (Button) findViewById( R.id.btnSearch );
+        btnChangeActivity = (Button) findViewById( R.id.btnChangActivity );
+        txtName = (TextView) findViewById( R.id.txtName );
+        txtCountry = (TextView) findViewById( R.id.txtCountry );
+        txtTemp = (TextView) findViewById( R.id.txtTemp );
+        txtStatus = (TextView) findViewById( R.id.txtStatus );
+        txtHumidity = (TextView) findViewById( R.id.txtHumidity );
+        txtCloud = (TextView) findViewById( R.id.txtCloud );
+        txtWind = (TextView) findViewById( R.id.txtWind );
+        txtDay = (TextView) findViewById( R.id.txtDay );
+        imgIcon = (ImageView) findViewById( R.id.imgIcon );
         btnSearch.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String city= edSearch.getText().toString();
-                GetCurrentWeatherData(city);
+                String city = edSearch.getText().toString();
+                GetCurrentWeatherData( city );
             }
         } );
     }
-    public void GetCurrentWeatherData(String data){
+
+    public void GetCurrentWeatherData(String data) {
         /*thực thi những request được gửi đi*/
-        RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
-        String url="https://api.openweathermap.org/data/2.5/weather?q="+data+"&units=metric&appid=5ceabeb9f84c5ce0b82a8d53243c81f6";
-        StringRequest stringRequest=new StringRequest( Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d("result",response);
-            }
-        }, new Response.ErrorListener() {
+        RequestQueue requestQueue = Volley.newRequestQueue( MainActivity.this );
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + data + "&units=metric&appid=5ceabeb9f84c5ce0b82a8d53243c81f6";
+        StringRequest stringRequest = new StringRequest( Request.Method.GET, url,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject( response );
+                        String day = jsonObject.getString( "date" );
+                        String name = jsonObject.getString( "name" );
+                        txtName.setText( "Name of City: " + name );
+                        Long l = Long.valueOf( day );
+                        Date date = new Date( l * 1000L );
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat( "EEEE dd-MM--yyyy HH-mm-ss" );
+                        String Day = simpleDateFormat.format( date );
+                        txtDay.setText( Day );
+                        JSONArray jsonArrayWeather = jsonObject.getJSONArray( "weather" );
+                        JSONObject jsonObjectWeather = jsonArrayWeather.getJSONObject( 0 );
+                        String status = jsonObjectWeather.getString( "main" );
+                        String icon = jsonObjectWeather.getString( "icon" );
+                        Picasso.with( MainActivity.this ).load( "https://openweathermap.org/img/wn/" + icon + ".png" ).into( imgIcon );
+                        txtStatus.setText( status );
+                        JSONObject jsonObjectMain = jsonObject.getJSONObject( "main" );
+                        String temp = jsonObjectMain.getString( "temp" );
+                        String humid = jsonObjectMain.getString( "humidity" );
+                        Double a = Double.valueOf( temp );
+                        String temperatue = String.valueOf( a.intValue() );
+                        txtTemp.setText( "temp: " + "°C" );
+                        txtHumidity.setText( "humid: " + "%" );
+                        JSONObject jsonObjectWind = jsonObject.getJSONObject( "wind" );
+                        String wind = jsonObjectWind.getString( "speed" );
+                        txtWind.setText( wind + "m/s" );
+                        JSONObject jsonObjectClouds = jsonObject.getJSONObject( "clouds" );
+                        String cloud = jsonObjectClouds.getString( "all" );
+                        txtCloud.setText( cloud + "%" );
+                        JSONObject jsonObjectSys = jsonObject.getJSONObject( "sys" );
+                        String country = jsonObjectSys.getString( "country" );
+                        txtCountry.setText( "Country name: " + country );
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
             }
         } );
-        requestQueue.add(stringRequest);
+        requestQueue.add( stringRequest );
     }
 
 
